d137071ebab91ca9a5e945c02d8c73cf0f9da508
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-03-03 16:05:44 +0100

    ✨ Guessing game.

---
+++ target/debug/deps/rand_chacha-7d519b4453f5ebd0.d
@@ -0,0 +1,9 @@
+C:\Users\antho\Desktop\rust\target\debug\deps\librand_chacha-7d519b4453f5ebd0.rmeta: C:\Users\antho\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\antho\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\antho\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\antho\Desktop\rust\target\debug\deps\librand_chacha-7d519b4453f5ebd0.rlib: C:\Users\antho\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\antho\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\antho\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\antho\Desktop\rust\target\debug\deps\rand_chacha-7d519b4453f5ebd0.d: C:\Users\antho\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\antho\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\antho\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\antho\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs:
+C:\Users\antho\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs:
+C:\Users\antho\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs:
---
+++ target/debug/deps/rustoooor-14a73496af5bfe42.d
@@ -0,0 +1,5 @@
+C:\Users\antho\Desktop\rust\target\debug\deps\librustoooor-14a73496af5bfe42.rmeta: src\main.rs
+
+C:\Users\antho\Desktop\rust\target\debug\deps\rustoooor-14a73496af5bfe42.d: src\main.rs
+
+src\main.rs:
